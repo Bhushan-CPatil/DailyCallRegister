efd8a17355e0c56435074cac38979086161816b5
Bhushan <pbhushan246@gmail.com>
2019-05-29 18:47:23 +0530

    files committed on 28-05-2019

--- app/src/main/java/com/eis/dailycallregister/Activity/CheckDCRSummary.java
+++ app/src/main/java/com/eis/dailycallregister/Activity/CheckDCRSummary.java
@@ -2,6 +2,7 @@ package com.eis.dailycallregister.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 
 import com.eis.dailycallregister.R;
 
@@ -10,6 +11,9 @@ public class CheckDCRSummary extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle(Html.fromHtml("<font color='#00E0C6'>DCR SUMMARY</font>"));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back_black);
         setContentView(R.layout.activity_check_dcrsummary);
     }
 }

79c930506ef331b92df4fbf6c1e7fcd1fba18e41
Bhushan <pbhushan246@gmail.com>
2019-06-12 18:54:00 +0530

    files committed on 12-06-2019

--- app/src/main/java/com/eis/dailycallregister/Activity/LoginScreen.java
+++ app/src/main/java/com/eis/dailycallregister/Activity/LoginScreen.java
@@ -14,6 +14,7 @@ import android.os.Vibrator;
 import android.provider.Settings;
 import android.support.design.button.MaterialButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -69,7 +70,8 @@ import retrofit2.Response;
 
 public class LoginScreen extends AppCompatActivity {
 
-    public EditText uid,pass,date;
+    public EditText uid,date;
+    public TextInputEditText pass;
     public Button login;
     DatePickerDialog datePickerDialog;
     ViewDialog progress;
